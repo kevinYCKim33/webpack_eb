4e7162d45ccef765d380ec646ca968cda2949109
Kevin Yongchul Kim <kevikim33@gmail.com>
2019-04-12 01:00:07 -0700

    drop hack

--- db/migrate/20190410044850_create_posts.rb
+++ db/migrate/20190410044850_create_posts.rb
@@ -1,11 +1,12 @@
-# class CreatePosts < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :posts do |t|
-#       t.string :title
-#       t.text :body
-#       t.boolean :published
-#
-#       t.timestamps
-#     end
-#   end
-# end
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :posts
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.boolean :published
+
+      t.timestamps
+    end
+  end
+end
